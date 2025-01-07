3b5dbdb04ad29f41dd9f0e1a0d9d546efb7ff3fb
Ajaybalajiprasad <ajaybalajiprasad02@gmail.com>
2025-01-07 23:03:01 +0530

    [FIX] Resposive Design

--- components/Codes.tsx
+++ components/Codes.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useMemo } from "react";
 import axios from "axios";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism";
@@ -13,13 +13,24 @@ interface Snippet {
 export default function Codes() {
   const [snippets, setSnippets] = useState<Snippet[]>([]);
   const [search, setSearch] = useState("");
+  const [debouncedSearch, setDebouncedSearch] = useState(search);
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
   const searchInputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedSearch(search.trim());
+    }, 300);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [search]);
+
+  useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
-      if (e.metaKey && e.key.toLowerCase() === "k") {
+      if ((e.metaKey || e.ctrlKey) && e.key.toLowerCase() === "k") {
         e.preventDefault();
         searchInputRef.current?.focus();
       }
@@ -29,6 +40,7 @@ export default function Codes() {
     return () => window.removeEventListener("keydown", handleKeyDown);
   }, []);
 
+  // Fetch snippets from the API
   useEffect(() => {
     const fetchSnippets = async () => {
       setLoading(true);
@@ -47,13 +59,21 @@ export default function Codes() {
     fetchSnippets();
   }, []);
 
-  const filteredSnippets = snippets.filter((snippet) =>
-    snippet.content.toLowerCase().includes(search.toLowerCase())
-  );
+  const filteredSnippets = useMemo(() => {
+    if (!debouncedSearch) return snippets;
+
+    const searchLower = debouncedSearch.toLowerCase();
+
+    return snippets.filter(
+      (snippet) =>
+        snippet.content.toLowerCase().includes(searchLower) ||
+        snippet.id.toLowerCase().includes(searchLower)
+    );
+  }, [snippets, debouncedSearch]);
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-800 via-gray-900 to-black text-white">
-      <div className="container mx-auto py-8">
+      <div className="container mx-auto py-8 lg:max-w-7xl">
         <h1 className="text-5xl font-bold text-center mb-6 text-indigo-500">
           C Exam Codes
         </h1>
@@ -71,10 +91,10 @@ export default function Codes() {
               placeholder="Search Codes by Question (or) Title..."
               value={search}
               onChange={(e) => setSearch(e.target.value)}
-              className="w-full p-2 rounded-lg shadow-lg border border-gray-600 bg-gray-700 text-gray-200 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-400"
+              className="w-full p-3 rounded-lg shadow-lg border border-gray-600 bg-gray-700 text-gray-200 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-400"
             />
             <span className="hidden lg:block text-sm text-gray-500 mt-1">
-              Press Windows + K to focus search
+              Press Ctrl/Cmd + K to focus search
             </span>
           </div>
         </div>
@@ -87,73 +107,76 @@ export default function Codes() {
             {error}
           </p>
         ) : (
-<ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-6 mt-10">
-  {filteredSnippets.map((snippet) => (
-    <li
-      key={snippet.id}
-      className="p-6 bg-gray-800 text-gray-200 rounded-lg shadow-lg"
-    >
-                <div className="flex justify-between items-center mb-3">
-                  <h2 className="text-xl font-semibold text-indigo-400">
-                    {snippet.id
-                      .replace(/_/g, " ")
-                      .split(".")
-                      .slice(0, -1)
-                      .join(".")}
-                  </h2>
-                  <div className="lg:flex space-x-2">
-                    <button
-                      onClick={() =>
-                        navigator.clipboard.writeText(snippet.content)
-                      }
-                      className="text-gray-400 hover:text-gray-200 focus:outline-none"
-                      aria-label="Copy code"
-                    >
-                      <MdContentCopy size={20} />
-                    </button>
-                    <button
-                      onClick={() => {
-                        if (navigator.share) {
-                          navigator
-                            .share({
-                              title: "C Exam Code Snippet",
-                              text: snippet.content,
-                              url: window.location.href,
-                            })
-                            .catch((error) =>
-                              console.error("Error sharing:", error)
-                            );
-                        } else {
-                          navigator.clipboard
-                            .writeText(window.location.href)
-                            .then(() => alert("Link copied to clipboard"))
-                            .catch((error) =>
-                              console.error("Error copying link:", error)
-                            );
-                        }
-                      }}
-                      className="text-gray-400 hover:text-gray-200 focus:outline-none"
-                      aria-label="Share code"
-                    >
-                      <MdShare size={20} />
-                    </button>
-                  </div>
-                </div>
-                <SyntaxHighlighter
-                  language="c"
-                  style={vscDarkPlus}
-                  className="rounded-md overflow-auto"
+          <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 xl:grid-cols-1 gap-6 mt-10">
+            {filteredSnippets.length > 0 ? (
+              filteredSnippets.map((snippet) => (
+                <li
+                  key={snippet.id}
+                  className="p-6 bg-gray-800 text-gray-200 rounded-lg shadow-lg"
                 >
-                  {snippet.content}
-                </SyntaxHighlighter>
-              </li>
-            ))}
+                  <div className="flex justify-between items-center mb-3">
+                    <h2 className="text-xl font-semibold text-indigo-400">
+                      {snippet.id
+                        .replace(/_/g, " ")
+                        .split(".")
+                        .slice(0, -1)
+                        .join(".")}
+                    </h2>
+                    <div className="hidden lg:flex space-x-2">
+                      <button
+                        onClick={() =>
+                          navigator.clipboard.writeText(snippet.content)
+                        }
+                        className="text-gray-400 hover:text-gray-200 focus:outline-none"
+                        aria-label="Copy code"
+                        title="Copy Code"
+                      >
+                        <MdContentCopy size={20} />
+                      </button>
+                      <button
+                        onClick={() => {
+                          if (navigator.share) {
+                            navigator
+                              .share({
+                                title: "C Exam Code Snippet",
+                                text: snippet.content,
+                                url: window.location.href,
+                              })
+                              .catch((error) =>
+                                console.error("Error sharing:", error)
+                              );
+                          } else {
+                            navigator.clipboard
+                              .writeText(window.location.href)
+                              .then(() => alert("Link copied to clipboard"))
+                              .catch((error) =>
+                                console.error("Error copying link:", error)
+                              );
+                          }
+                        }}
+                        className="text-gray-400 hover:text-gray-200 focus:outline-none"
+                        aria-label="Share code"
+                        title="Share Code"
+                      >
+                        <MdShare size={20} />
+                      </button>
+                    </div>
+                  </div>
+                  <SyntaxHighlighter
+                    language="c"
+                    style={vscDarkPlus}
+                    className="rounded-md overflow-auto"
+                  >
+                    {snippet.content}
+                  </SyntaxHighlighter>
+                </li>
+              ))
+            ) : (
+              <p className="col-span-full px-4 text-center text-xl font-medium mt-12 text-gray-400">
+                No Codes found!! No worries, ask Ajay to add it :)
+              </p>
+            )}
           </ul>
-        )}
-        {!loading && !error && filteredSnippets.length === 0 && (
-          <p className="text-center text-xl font-medium mt-12 text-gray-400">
-            No Codes found!! No worries Ask Ajay to Add it :)
-          </p>
         )}
       </div>
     </div>
